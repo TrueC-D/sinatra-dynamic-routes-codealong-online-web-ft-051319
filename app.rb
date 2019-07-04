3c221daea0182e67bdddc0c2b716a547a9467694
Alethea Cary <aletheacary@gmail.com>
2019-07-04 19:47:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,6 +21,6 @@ class App < Sinatra::Base
     @number1 = params[:num1]
     @number1 = params[:num2]
     @product = @number1.to_i*@number2.to_i
-    "#{number1}*#{number2} = #{@product}"
+    "#{@number1}*#{@number2} = #{@product}"
   end
 end
